6ba952566ad12d061616cb0637ed22514442a32f
jbxamora <jbxamora@icloud.com>
2023-01-16 19:38:18 -0800

    created basic functions

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,6 @@
+// Import Data From My Question Bank
 import questionBank from "./Qbank";
-
+// Declare Variables
 let startBtn = document.querySelector("#strbtn");
 let timer = document.querySelector("#timer");
 let time = document.querySelector("#time");
@@ -18,3 +19,28 @@ let highscoreForm = document.querySelector("#highform");
 let highscoreList = document.querySelector("#highlist");
 let clearScore = document.querySelector("#clearscore");
 
+function setQuestionText(questionText) {
+    document.getElementById("question").textContent = questionText
+}
+
+function setQuestionChoices(Choices) {
+    Choices.innerHTML = " ";
+    for (var i in Choices) {
+        var Choices = document.createElement("button");
+        Choices.addEventListener("click", function (event){
+           
+        })
+    }
+}
+function setTimer() {
+    timeInterval = setInterval(function () {
+        mainTimer--;
+        timer.textContent = "Time Remaining: " + mainTimer;
+        if (mainTimer === 0) {
+            clearInterval(timeInterval);
+            endQuiz();
+
+        }
+        
+    }, 1000)
+}
