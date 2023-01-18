e324c898f8b6839aef4f08caaf52dbb344a289f7
jbxamora <jbxamora@icloud.com>
2023-01-17 19:08:57 -0800

    broken app, everytime i press start the question fills in again

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,25 +1,24 @@
 // Import Data From My Question Bank
-import questionBank from "./Qbank";
-console.log(questionBank)
+// import questionBank from "./Qbank";
+// console.log(questionBank)
 // Declare Variables
 var startBtn = document.querySelector("#strbtn");
 var restartBtn = document.querySelector("#restartbtn");
 var timer = document.querySelector("#timer");
-var time = document.querySelector("#time");
 var mainTimer = 61;
 var questionInit = 0;
 var highscores = [];
 var timeInterval = "";
-var choices = document.querySelector("#Choices");
-var correctAnswer = document.querySelector("#checkAnswer");
+// var correctAnswer = document.querySelector("#checkAnswer");
 var decision = document.querySelector("#decision");
 var main = document.querySelector("#container");
-var questionEl = document.querySelector("#question")
+// var questionEl = document.querySelector("#question")
 var highscore = document.querySelector("#hiddenhs");
 var highscoreInput = document.querySelector("#initials");
 var highscoreForm = document.querySelector("#highform");
 var highscoreList = document.querySelector("#highlist");
 var clearScore = document.querySelector("#clearscore");
+var questionCount = 0;
 
 
 // Event Listener for Start Button 
@@ -36,6 +35,9 @@ function startQuiz() {
 restartBtn.addEventListener("click", restartQuiz);
 // Restart Quiz Function
 function restartQuiz() {
+    main.setAttribute("id", "container");
+    highscore.setAttribute("id", "hiddenhs")
+    highscoreForm.setAttribute("id", "highform")
     mainTimer = 61;
     questionInit = 0;
     setTimer();
@@ -50,16 +52,16 @@ function setQuestionText(questionText) {
     document.getElementById("question").textContent = questionText
 }
 
-function setQuestionChoices(Choices) {
-    Choices.innerHTML = " ";
-    for (var i in Choices) {
-        var Choices = document.createElement("button");
-        Choices.addEventListener("click", function (event){
-            
+function setQuestionChoices(choices) {
+    choices.innerHTML = " ";
+    for (var i in choices) {
+        var ansChoice = document.createElement("button");
+        ansChoice.addEventListener("click", function (event){
+        ChoiceMade(event.target);
         });
-        var answerChoices = document.createTextNode(answerChoices[i]);
-        decision.appendChild(answerChoices);
-        answerChoices.appendChild(answerChoices);
+        var ansChoices = document.createTextNode(choices[i]);
+        decision.appendChild(ansChoices);
+        choices.appendChild(choices);
     }
 }
 
@@ -143,3 +145,95 @@ function endQuiz() {
     questionCount = 0;
     clearInterval(timeInterval);
 }
+
+const questionBank = [
+  {
+    questionText: "What does the method split do to a string?",
+    answerChoices: [
+      "splits the last character off the string",
+      "deconstructs the string and places it into an array",
+      "splits the first character off the string",
+      "splits the string into two user selected variables",
+    ],
+    correctAnswer: "deconstructs the string and places it into an array",
+  },
+  {
+    questionText:
+      "Where is the correct place to insert a JavaScript in an HTML document?",
+    answerChoices: [
+      "Both the <head> section and the <body> section are correct",
+      "The bottom of the <body> section",
+      "The <head> section",
+      "The footer section",
+    ],
+    correctAnswer: "The bottom of the <body> section",
+  },
+  {
+    questionText: 'How do you call a function named "myFunction"?',
+    answerChoices: [
+      "call myFunction()",
+      "call function myFunction",
+      "Call.myFunction()",
+      "myFunction()",
+    ],
+    correctAnswer: "myFunction()",
+  },
+  {
+    questionText: "How can you add a comment in javaScript?",
+    answerChoices: [
+      "//This is a comment",
+      "'This is a comment",
+      "<!--This is a comment",
+      "#This is a comment",
+    ],
+    correctAnswer: "//This is a comment",
+  },
+  {
+    questionText: "How do you find the largest number of 2 and 4?",
+    answerChoices: ["Math.Ceil(2,4)", "Math.max(2,4)", "ceil(2,4)", "top(2,4)"],
+    correctAnswer: "Math.max(2,4)",
+  },
+  {
+    questionText: "In JavaScript, the symbols + - * and / are:",
+    answerChoices: [
+      "operators",
+      "expressions",
+      "comparison operators",
+      "None of the Above",
+    ],
+    correctAnswer: "operators",
+  },
+  {
+    questionText:
+      "When you want to use JavaScript to manipulate the currently displayed Web page, the Web page's javaScript object name is:",
+    answerChoices: ["Frame", "Document", "Window", "browser_window"],
+    correctAnswer: "Document",
+  },
+  {
+    questionText: "In JavaScript, the expression x!=y returns false if:",
+    answerChoices: [
+      "the variables are equal",
+      "x is less than y",
+      "the variables are not equal",
+      "None of the above",
+    ],
+    correctAnswer: "the variables are equal",
+  },
+  {
+    questionText:
+      "When you want to use JavaScript to manipulate the browser window, the browser window's JavaScript object name is:",
+    answerChoices: ["Frame", "Document", "Window", "browser_window"],
+    correctAnswer: "Window",
+  },
+  {
+    questionText:
+      'In JavaScript, what would be the proper form of address in the object hierarchy for the second element in a form called "info"?',
+    answerChoices: [
+      "document.info.elements[1]",
+      "document.info.elements[2]",
+      "document.forms.info.elements[2]",
+      "info.elements[2]",
+    ],
+    correctAnswer: "document.info.elements[1]",
+  },
+]; 
